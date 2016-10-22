569eb4625f14a762d00cf0f8ab202b41d17d1b41
Ian Douglas Scott <ian@iandouglasscott.com>
2016-10-22 10:45:54 -0700

    Pass -v to rsync

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,4 +4,4 @@ set -e
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key.pem
 ssh-add .travis/deploy_key.pem
-rsync -e "ssh -o 'StrictHostKeyChecking no'" -r --delete docs/html/ travis@iandouglasscott.com:/srv/naevdoc
+rsync -e "ssh -o 'StrictHostKeyChecking no'" -rv --delete docs/html/ travis@iandouglasscott.com:/srv/naevdoc
