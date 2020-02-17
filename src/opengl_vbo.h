ce0e6622188b1c3223fa8faeab163fd00ec56093
Ian Douglas Scott <ian@iandouglasscott.com>
2020-02-16 17:48:12 -0800

    Remove legacy gl_vboActivateOffset() and gl_vboActivate() functions
    These are replaced with gl_vboActivateAttribOffset() for modern OpenGL.

--- src/opengl_vbo.h
+++ src/opengl_vbo.h
@@ -36,9 +36,6 @@ void gl_vboData( gl_vbo *vbo, GLsizei size, void* data );
 void gl_vboSubData( gl_vbo *vbo, GLint offset, GLsizei size, void* data );
 void* gl_vboMap( gl_vbo *vbo );
 void gl_vboUnmap( gl_vbo *vbo );
-void gl_vboActivate( gl_vbo *vbo, GLuint class, GLint size, GLenum type, GLsizei stride );
-void gl_vboActivateOffset( gl_vbo *vbo, GLuint class, GLuint offset,
-      GLint size, GLenum type, GLsizei stride );
 void gl_vboActivateAttribOffset( gl_vbo *vbo, GLuint index, GLuint offset,
       GLint size, GLenum type, GLsizei stride );
 void gl_vboDeactivate (void);
